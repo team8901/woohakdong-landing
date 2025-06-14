bfeaf8d0b36c764ccc02a2a5d3d343f8987e3468
salmonco <xkqjsslsek80@naver.com>
2025-06-14 23:40:53 +0900

    Feat: 섹션 구성 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { About } from "./components/About";
-import { Cta } from "./components/Cta";
+import { CenteredHero } from "@/components/CenteredHero";
+import "./App.css";
 import { FAQ } from "./components/FAQ";
 import { Features } from "./components/Features";
 import { Footer } from "./components/Footer";
-import { Hero } from "./components/Hero";
-import { HowItWorks } from "./components/HowItWorks";
 import { Navbar } from "./components/Navbar";
-import { Newsletter } from "./components/Newsletter";
-import { Pricing } from "./components/Pricing";
 import { ScrollToTop } from "./components/ScrollToTop";
-import { Services } from "./components/Services";
-import { Sponsors } from "./components/Sponsors";
-import { Team } from "./components/Team";
-import { Testimonials } from "./components/Testimonials";
-import "./App.css";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
-      <Sponsors />
-      <About />
-      <HowItWorks />
+      {/* <Hero /> */}
+      <CenteredHero />
+      {/* <Sponsors /> */}
+      {/* <About /> */}
+      {/* <HowItWorks /> */}
       <Features />
-      <Services />
-      <Cta />
-      <Testimonials />
-      <Team />
-      <Pricing />
-      <Newsletter />
+      {/* <Services /> */}
+      {/* <Cta /> */}
+      {/* <Testimonials /> */}
+      {/* <Team /> */}
+      {/* <Pricing /> */}
+      {/* <Newsletter /> */}
       <FAQ />
       <Footer />
       <ScrollToTop />
